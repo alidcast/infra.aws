d7289d800a350be18c32d3a72d4e8f6142788301
Alid <alidcastano@gmail.com>
2020-04-04 22:46:49 -0500

    add eid tag to specificy env specific names

--- src/rejure/infra/aws.clj
+++ src/rejure/infra/aws.clj
@@ -14,31 +14,34 @@
   "Convert paramter map to AWS paramater array."
   [params]
   (into [] (for [[k v] params]
-             {:ParameterKey (name k)
+             {:ParameterKey   (name k)
               :ParameterValue v})))
 
-(defn serialize-opts
-  [template env]
+(defn serialize-config
+  "Expects mapping of resource names to template options.
+   A template options can either be a vector, if template is dervied from a url, or a map.
+   For vector first arg is url string, second is parameter map, and third is extra aws options
+   The key-value parameters are serialized to match aws spec and merged with the extra aws options."
+  [template]
   (reduce-kv
    (fn [m k v]
-     (let [stack-name (str (name k) "-" (name env))
-           template   (if (url-template? v)
-                          (let [[url params extra-opts] v]
-                            (merge {:StackName stack-name
-                                    :TemplateURL url
-                                    :Parameters (serialize-params params)}
-                                   extra-opts))
-                          {:StackName stack-name
-                           :TemplateBody v
+     (let [template   (if (url-template? v)
+                        (let [[url params extra-opts] v]
+                          (merge {:StackName   k
+                                  :TemplateURL url
+                                  :Parameters  (serialize-params params)}
+                                 extra-opts))
+                        {:StackName    k
+                         :TemplateBody v
                            ;; (json/write-str v)
-                           })]
-       (assoc m stack-name template)))
+                         })]
+       (assoc m k template)))
    {}
    template))
 
 ;; -- Custom `reader` tags \start
 (defmethod reader 'param
-  ;; Replace param key with value.
+  ;; Replace param key with value. 
   ;; i.e. #param :foo -> "bar"
   [{:keys [params]} _ k]
   (get params k))
@@ -49,22 +52,22 @@
   [_ _ [fmt & args]]
   (apply format fmt args))
 
-(defmethod reader 'stack
-  [_ _ k]
-  (str (name k) "-dev"))
+(defmethod reader 'eid
+  ;; Append environment info to identifier key.
+  ;; i.e. #eid :foo -> "foo-dev"
+  [{:keys [profile]} _ k]
+  (str (name k) "-" (name profile)))
 ;; -- Custom `reader` tags \end
 
 (defn read-config
   "Read an AWS resource configuration.
    Takes a resource path `path`, env keyword `env` and parameter map `params` as arguments.
-   Expects edn config to be mapping of resource names to template options.
-   Returns that same mapping of resources with their template options serialized to match aws spec.
-   See `serialize-opts` for templating details."
+   Returns mapping of resources with their template options serialized to match aws spec.
+   See `serialize-config` for templating details."
   ([path env] (read-config path env {}))
   ([path env params]
-   (serialize-opts (aero/read-config (io/resource path) {:profile env
-                                                         :params  params})
-                       env)))
+   (serialize-config (aero/read-config (io/resource path) {:profile env
+                                                           :params  params}))))
 
 ;; # Cloudformation Operations
 
@@ -73,13 +76,12 @@
   (aws/client {:api :cloudformation}))
 
 (defn- req-clf
-  "Make aws cloudformation request."
+  "Make an aws cloudformation request."
   [op request]
   (aws/invoke clf-client {:op op
                           :request request}))
 
 (defn create-stack
-  "Creates a stack, either as a template url or map of options."
   [opts]
   (req-clf :CreateStack opts))
 
