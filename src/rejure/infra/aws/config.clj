d8be0d4b064ae91df49f660a5f8ea2987dfa5969
Alid <alidcastano@gmail.com>
2020-04-07 22:19:48 -0500

    serialize template body to json in create-stack operation; cleanup docstrings/comments

--- src/rejure/infra/aws/config.clj
+++ src/rejure/infra/aws/config.clj
@@ -1,9 +1,7 @@
 (ns rejure.infra.aws.config
-  "Configure cloudformation templates via EDN."
-  (:require 
-  ;;  [clojure.data.json :as json]
-   [clojure.edn :as edn]
-   [clojure.string :as string]))
+  "Configure AWS Cloudformation templates with EDN."
+  (:require [clojure.edn :as edn]
+            [clojure.string :as string]))
 
 ;; # Config Helpers
 
@@ -13,36 +11,36 @@
   [ident env]
   (str (name ident) "-" (name env)))
 
-
 ;; # Config Serializer
 
 (defn- template-url? [v]
   (vector? v))
 
-(defn- ->template-url-map
+(defn- ->aws-template-url
   "Accepts stack name keyword `k` and template `url` string."
   [k url]
   {:StackName   k
    :TemplateURL url})
 
-(defn- ->resource-type
+(defn- ->aws-resource-type
   "Converts resource key `k` to AWS identifier type.
    The key should be in ':<Service>.<Module>' format.
    i.e. :Service.Module -> AWS::Service::Module"
   [k]
   (string/join "::" (cons "AWS" (string/split (name k) #"\."))))
 
-(defn- ->template-body-map
-  "Accepts stack name keyword `k` and template map `body`."
+(defn- ->aws-template-body
+  "Accepts stack name keyword `k` and template map `body`.
+   Expands a resource's tuple shorthand into AWS type+properties map."
   [k body]
   {:StackName    k
    :TemplateBody (assoc body 
                         :Resources 
-                        (reduce-kv (fn [rm rk rv]
-                                     (assoc rm  rk (if (vector? rv)
-                                                     {:Type (->resource-type (first rv))
-                                                      :Properties (last rv)}
-                                                     rv)))
+                        (reduce-kv (fn [m k v]
+                                     (assoc m  k (if (vector? v)
+                                                     {:Type (->aws-resource-type (first v))
+                                                      :Properties (second v)}
+                                                     v)))
                                    {}
                                    (:Resources body)))})
 
@@ -59,9 +57,9 @@
    (fn [m k v]
      (let [template   (if (template-url? v)
                         (let [[url clf-opts] v]
-                          (merge (->template-url-map k url)
+                          (merge (->aws-template-url k url)
                                  clf-opts))
-                        (->template-body-map k v))]
+                        (->aws-template-body k v))]
        (assoc m k template)))
    {}
    config))
@@ -69,7 +67,7 @@
 ;; # Config Reader Literals
 
 (defn- kv-params->aws-params
-  "Convert map `m` to AWS paramater array."
+  "Convert param map `m` to AWS paramater array."
   [m]
   (into [] (for [[k v] m] {:ParameterKey (name k) :ParameterValue v})))
 
@@ -78,23 +76,17 @@
   [k]
   {:Ref (name k)})
 
-(defn- with-system-param-resources
-  "Takes resource map `m` and add a sytem parameter resource for each identifier key."
+(defn- with-aws-ssm-param-resources
+  "Include a sytem manager parameter resource for each key in resource map."
   [m]
   (reduce-kv
    (fn [acc k _]
      (assoc acc
             (keyword (str (name k) "Param"))
-            [:SSM.Parameter {:Name (name k) :Value (k->aws-resource-ref k)}]))
+            [:SSM.Parameter {:Name (name k) :Value (k->aws-resource-ref k) :Type "String"}]))
    m
    m))
 
-(comment 
-  (with-system-param-resources {:AuthPool {}
-                                :AuthPoolClient {}}))
-
-(comment 
-  )
 (defn create-readers 
   "Create edn reader literals.
    Where appropriate we match the AWS function utilities provided for YAML/JSON templates.
@@ -104,10 +96,11 @@
    'kvp kv-params->aws-params
    'ref k->aws-resource-ref
    'sub (fn [k] (get params k))
-   'with-params with-system-param-resources})
+   'with-ssm-params with-aws-ssm-param-resources})
 
 ;; # Config Reader 
 
+;; TODO prefix resources with clf 
 (defn read-edn
   "Read an AWS edn configuration.
    Takes an edn string `s`, env keyword `env` and parameter map `params` as arguments.
--- src/rejure/infra/aws/request.clj
+++ src/rejure/infra/aws/request.clj
@@ -1,27 +1,39 @@
 (ns rejure.infra.aws.request
-  "Send AWS Cloudformation requests."
-  (:require [cognitect.aws.client.api :as aws]))
+  "Request AWS Cloudformation operations."
+  (:require [clojure.data.json :as json]
+            [cognitect.aws.client.api :as aws]))
 
 ;; # Cloudformation Operations 
 
-(def ^:private clf-client 
-  "Cloudformation client."
+(def clf-client
+  "Clouformation client."
   (aws/client {:api :cloudformation}))
 
 (defn- req-clf
-  "Make an Cloudformation request."
-  [op request]
+  "Request Cloudformation operation `op` with options `opts`."
+  [op opts]
   (aws/invoke clf-client {:op op
-                          :request request}))
+                          :request opts}))
+
+(defn- serialize-opts 
+  "If template body is present parse it to JSON; leave other options as is."
+  [opts]
+  (if (:TemplateBody opts)
+    (assoc opts
+           :TemplateBody 
+           (json/write-str (:TemplateBody opts)))
+    opts))
 
 (defn create-stack
   [opts]
-  (req-clf :CreateStack opts))
+  (req-clf :CreateStack (serialize-opts opts)))
 
-(defn describe-stack 
+(defn describe-stack
   [opts]
   (req-clf :DescribeStack opts))
 
+;; TODO add guard for deleting production stacks
 (defn delete-stack [name]
   (req-clf :DeleteStack {:StackName name
                          :RetainResources []}))
+
