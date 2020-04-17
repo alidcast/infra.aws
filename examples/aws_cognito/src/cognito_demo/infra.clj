a8054b697115c34f6b66dfb6978c377490a89f21
Alid <alidcastano@gmail.com>
2020-04-17 00:05:36 -0500

    fix examples

--- examples/aws_cognito/src/cognito_demo/infra.clj
+++ examples/aws_cognito/src/cognito_demo/infra.clj
@@ -2,4 +2,4 @@
   (:require [rejure.infra.aws.config :as ic]
             [clojure.java.io :as io]))
 
-(def cfg (ic/read-edn (io/reader "resources/demo/aws-cognito-config.edn") :dev {}))
+(def cfg (ic/read-edn (slurp (io/reader "resources/cognito_demo/aws-stacks.edn")) :dev))
--- examples/datomic/src/datomic_demo/infra.clj
+++ examples/datomic/src/datomic_demo/infra.clj
@@ -2,4 +2,4 @@
   (:require [rejure.infra.aws.config :as ic]
             [clojure.java.io :as io]))
 
-(def cfg (ic/read-edn (io/reader "resources/demo/datomic-stacks.edn") :dev {}))
+(def cfg (ic/read-edn (slurp (io/reader "resources/datomic_demo/aws-stacks.edn")) :dev))
