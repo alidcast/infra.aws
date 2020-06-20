3aa65c3f1c888293883f4a3e0843765d399e3939
Alid <alidcastano@gmail.com>
2020-06-20 10:04:36 -0500

    allow passing extra reader literals

--- infra.aws/src/infra/aws/configure.clj
+++ infra.aws/src/infra/aws/configure.clj
@@ -85,9 +85,11 @@
   [m]
   (into [] (for [[k v] m] {:ParameterKey (name k) :ParameterValue v})))
 
-(defn- k->aws-resource-ref "Coverts key `k` to AWS logical resource reference."
-  [k]
-  {:Ref (name k)})
+(defn- k->aws-ref "Coverts key `k` to AWS logical resource reference."
+  [k] {"Ref" (name k)})
+
+(defn k->aws-import-value "Convert key `k` to AWS import value stack reference function."
+  [k] {"Fn::ImportValue" k})
 
 (defn- with-aws-ssm-params 
   "Auto-includes a Sytem Manager Parameter for each declared resource identifier in map `m`."
@@ -97,7 +99,7 @@
      (assoc acc
             (keyword (str (name k) "Param"))
             {:Type "AWS::SSM::Parameter"
-             :Properties {:Name (name k) :Type "String" :Value (k->aws-resource-ref k)}}))
+             :Properties {:Name (name k) :Type "String" :Value (k->aws-ref k)}}))
    m
    m))
 
@@ -109,15 +111,20 @@
    'aws/params kv-params->aws-params
    'aws/with-ssm-params with-aws-ssm-params
    ;; aws built-ins
-   'aws/ref k->aws-resource-ref
+   'aws/ref k->aws-ref
+   'aws/import k->aws-import-value
    'aws/sub (fn [k] (get params k))})
 
 ;; # Config Reader
 
+(def default-opts {:params {}
+                   :extra-readers {}})
 (defn read-edn
   "Reads config edn string `s` based on environment keyword `env` and an optional `params` map.
    See `serialize-config` for templating details."
-  ([s env] (read-edn s env {}))
-  ([s env params]
-   (serialize-config (edn/read-string {:readers (create-readers env params)}
-                                      s))))
+  ([s env] (read-edn s env default-opts))
+  ([s env opts]
+   (let [{:keys [params extra-readers]} (merge default-opts opts)]
+    (serialize-config
+     (edn/read-string {:readers (merge (create-readers env params) extra-readers)}
+                      s)))))
